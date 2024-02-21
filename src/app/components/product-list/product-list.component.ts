d5b88dd4c1b567745e99f611aff9a12c179d8456
Livia <liviahelen025@gmail.com>
2024-02-20 22:16:09 -0300

    Create product list component.

---
+++ src/app/components/product-list/product-list.component.ts
@@ -0,0 +1,35 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { Product } from '../../types/products';
+import { Observable } from 'rxjs';
+import { ProductService } from '../../services/product.service';
+import { ProductAlertsComponent } from '../product-alerts/product-alerts.component';
+
+@Component({
+  selector: 'app-product-list',
+  standalone: true,
+  imports: [
+    CommonModule,
+    ProductAlertsComponent
+  ],
+  templateUrl: './product-list.component.html',
+  styleUrl: './product-list.component.scss'
+})
+
+export class ProductListComponent {
+  products: Observable<Product[]> = new Observable<Product[]>();
+
+  constructor(private productService: ProductService) { }
+
+  ngOnInit() {
+    this.products = this.productService.getProducts()
+  };
+
+  share() {
+    alert('The product has been shared!');
+  }
+
+  onNotify() {
+    alert('You will be notified when the product goes on sale');
+  }
+}
