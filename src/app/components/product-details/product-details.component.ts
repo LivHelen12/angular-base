e763ef7f81a9e8c68b310c813f5dc400f9f499c1
Livia <liviahelen025@gmail.com>
2024-02-20 23:05:02 -0300

    Create product details.

---
+++ src/app/components/product-details/product-details.component.ts
@@ -0,0 +1,40 @@
+import { ActivatedRoute } from '@angular/router';
+import { Product } from './../../types/products';
+import { Component, } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ProductService } from '../../services/product.service';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-product-details',
+  standalone: true,
+  imports: [
+    CommonModule,
+  ],
+  templateUrl: './product-details.component.html',
+  styleUrl: './product-details.component.scss'
+})
+export class ProductDetailsComponent {
+  products: Observable<Product[]> = new Observable<Product[]>();
+  product: Product | undefined;
+
+  constructor(private route: ActivatedRoute, private productService: ProductService) { }
+
+  ngOnInit() {
+    this.products = this.productService.getProducts();
+
+    // Obtem o id do produto através do parâmetro de rota
+    const routeParams = this.route.snapshot.paramMap;
+    const productIdFromRoute = Number(routeParams.get('productId'));
+
+    // Encontra o produto que corresponde ao id do produto fornecido na rota.
+    this.products = this.productService.getProducts();
+    this.products.subscribe(products => {
+      this.product = products.find(product => product.id === productIdFromRoute);
+    });
+  }
+
+  ngOnDestroy() {
+    this.products.subscribe().unsubscribe();
+  }
+}
