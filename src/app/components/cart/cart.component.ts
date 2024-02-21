9191ba9f0040a78371661f9b8cb206f35d8a79b4
Livia <liviahelen025@gmail.com>
2024-02-21 12:17:50 -0300

    Create a form group.

--- src/app/components/cart/cart.component.ts
+++ src/app/components/cart/cart.component.ts
@@ -2,19 +2,38 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { CartService } from '../../services/cart.service';
 import { RouterModule } from '@angular/router';
-
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 @Component({
   selector: 'app-cart',
   standalone: true,
   imports: [
     CommonModule,
-    RouterModule
+    RouterModule,
+    ReactiveFormsModule
   ],
   templateUrl: './cart.component.html',
   styleUrl: './cart.component.scss'
 })
+
 export class CartComponent {
   items = this.cartService.getItems();
+  checkoutForm!: FormGroup;
 
-  constructor(private cartService: CartService) { }
+  constructor(private cartService: CartService, private formBuilder: FormBuilder) { }
+
+  ngOnInit() {
+    this.checkoutForm = this.formBuilder.group({
+      name: null,
+      address: this.formBuilder.group({
+        street: null,
+        number: null,
+      }),
+    });
+  }
+
+  onSubmit() {
+    this.items = this.cartService.clearCart();
+    console.log('Your order has been submitted', this.checkoutForm.value);
+    this.checkoutForm.reset();
+  }
 }
